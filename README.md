83047b853de93b72066e64c3e6e10a7e0f3a90c1
fernandoadms <fernando.alves0189@gmail.com>
2023-04-29 01:25:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-2023
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-j7enz9)
---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <main>
+      <h1>React 2023</h1>
+
+      <p>Here will be some things to study about React in 2023</p>
+
+      <ul>
+        <li>React 18</li>
+        <li>Tailwind</li>
+        <li>Hooks</li>
+        <li>Custom Hooks</li>
+        <li>React Testing Library</li>
+        <li>Typescript</li>
+        <li>Jest</li>
+        <li></li>
+      </ul>
+
+      <footer>
+        <ul>
+          <li>
+            <a href="https://jsonplaceholder.typicode.com/">
+              https://jsonplaceholder.typicode.com/
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </main>
+  );
+}
+
+export default App;
