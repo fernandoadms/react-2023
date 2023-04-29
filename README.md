83be3984b12ea9dd78ae3d7f475f6f123371b69f
fernandoadms <fernando.alves0189@gmail.com>
2023-04-29 02:28:31 -0300

    2 files modified

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# react-2023
+# React 2023
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-j7enz9)
+Modern React 2023
+
+> Keep in mind in 2023
+
+## Embrace
+
+- Custom Hooks
+- Cypress
+- Hooks
+- Jest
+- React 18
+- React Testing Library
+- Tailwind
+- Typescript
+- Best Practices
+- GraphQL
+- Redux
+
+## Courses
+
+### Free
+
+- [https://fullstackopen.com/ptbr](https://fullstackopen.com/ptbr)
+- [https://react.dev/](https://react.dev/)
+- [https://cult.honeypot.io/reads/8-best-react-courses-2022/](https://react.dev/)
+
+## Projects
+
+- [https://cult.honeypot.io/reads/8-best-react-courses-2022/](https://cult.honeypot.io/reads/8-best-react-courses-2022/)
+
+---
+
+### Links
+
+- https://jsonplaceholder.typicode.com/
+- https://blog.logrocket.com/modern-api-data-fetching-methods-react/
+- https://initjs.org/react-js-best-practices-fbd180e665b2
+- https://github.com/airbnb/javascript/tree/master/react
--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,7 @@
 function App() {
   return (
     <main>
-      <h1>React 2023</h1>
-
-      <p>Modern React 2023</p>
-
-      <ul>
-        <li>Custom Hooks</li>
-        <li>Cypress</li>
-        <li>Hooks</li>
-        <li>Jest</li>
-        <li>React 18</li>
-        <li>React Testing Library</li>
-        <li>Tailwind</li>
-        <li>Typescript</li>
-        <li>Best Practices</li>
-        <li>GraphQL</li>
-        <li>Redux</li>
-      </ul>
-
-      <div>
-        <h2>Courses</h2>
-
-        <h3>Free</h3>
-        <ul>
-          <li>
-            <a href="#">https://fullstackopen.com/ptbr</a>
-          </li>
-          <li>
-            <a href="#">https://react.dev/</a>
-          </li>
-          <li>
-            <a href="#">
-              https://cult.honeypot.io/reads/8-best-react-courses-2022/
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div>
-        <h3>Tests</h3>
-        <ul>
-          <li>
-            <a href="#">
-              https://cult.honeypot.io/reads/8-best-react-courses-2022/
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <footer>
-        <h3>Links</h3>
-        <ul>
-          <li>
-            <a href="#">https://jsonplaceholder.typicode.com/</a>
-          </li>
-          <li>
-            <a href="#">
-              https://blog.logrocket.com/modern-api-data-fetching-methods-react/
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              https://initjs.org/react-js-best-practices-fbd180e665b2
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              https://github.com/airbnb/javascript/tree/master/react
-            </a>
-          </li>
-        </ul>
-      </footer>
+      <h1>Hands On</h1>
     </main>
   );
 }
